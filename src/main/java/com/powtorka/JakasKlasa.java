c253a88efbcce0c7e2dffdf3810c01b430892522
Natalia Wszelaki <natalia.slocinska@gmail.com>
2023-05-01 14:06:32 +0200

    powtorka potrzebna

---
+++ src/main/java/com/powtorka/JakasKlasa.java
@@ -0,0 +1,4 @@
+package com.powtorka;
+
+public class JakasKlasa {
+}
---
+++ src/main/java/com/powtorka/PowtorkaApplication.java
@@ -0,0 +1,13 @@
+package com.powtorka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowtorkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowtorkaApplication.class, args);
+	}
+
+}
